bcbdbe18b7583d7d8ad882d109b04162c7712856
MALWICK <batahumphrey66@gmail.com>
2023-02-24 09:53:49 +0100

    creating navbar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -14,6 +14,22 @@ function Navbar() {
           <li>Contact</li>
         </ul>
       </div>
+      <div className={Styles.menu__options}>
+        <div className={Styles.iconMenu}>
+          <div className={Styles.menu__icon}>
+            <i className="fa-solid fa-bars" />
+          </div>
+        </div>
+        <div className={Styles.dropdown}>
+          <ul>
+            <li>Home</li>
+            <li>Company</li>
+            <li>Resources</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
