20efea206723ea33fd3f3e3d7b3124e2940188ad
MALWICK <batahumphrey66@gmail.com>
2023-02-25 22:37:11 +0100

    creating my navbar with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Hero from './component/Hero';
 import Navbar from './component/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
