9365aca9dac8a51382c642592a3d011f627e5773
MALWICK <batahumphrey66@gmail.com>
2023-02-25 05:45:44 +0100

    creating my navbar with tailwind

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,35 +1,16 @@
 import React from 'react';
-import Styles from './Navbar.module.css';
 
 function Navbar() {
   return (
-    <div className={Styles.navbar}>
-      <h1>React.</h1>
-      <div className={Styles.list}>
-        <ul>
-          <li>Home</li>
-          <li>Company</li>
-          <li>Resources</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      <div className={Styles.menu__options}>
-        <div className={Styles.iconMenu}>
-          <div className={Styles.menu__icon}>
-            <i className="fa-solid fa-bars" />
-          </div>
-        </div>
-        <div className={Styles.dropdown}>
-          <ul>
-            <li>Home</li>
-            <li>Company</li>
-            <li>Resources</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-      </div>
+    <div className="flex justify-between items-center h-24 max-w-[1240px] text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a] ">React.</h1>
+      <ul className="flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
     </div>
   );
 }
