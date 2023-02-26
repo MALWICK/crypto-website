56f517a6972e86e193e3bbe6f4c9d010c8845f24
MALWICK <batahumphrey66@gmail.com>
2023-02-26 07:07:28 +0100

    creating hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import Analytics from './component/Analytics';
 import Hero from './component/Hero';
 import Navbar from './component/Navbar/Navbar';
+import Newsletter from './component/Newsletter';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Analytics />
+      <Newsletter />
     </div>
   );
 }
