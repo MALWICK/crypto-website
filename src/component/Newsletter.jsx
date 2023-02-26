bda4765aae74cab106b33149cd938f127693982b
MALWICK <batahumphrey66@gmail.com>
2023-02-26 07:31:50 +0100

    creating my newsletter section

--- src/component/Newsletter.jsx
+++ src/component/Newsletter.jsx
@@ -4,11 +4,20 @@ function Newsletter() {
   return (
     <div className=" w-full py-16 text-white">
       <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
-        <div>
-          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold">
+        <div className="lg:col-span-2 ">
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
             Wants tips & tricks to optimize your flow?
           </h1>
           <p>Sign uo to our Newsletter and stay up to date</p>
+        </div>
+        <div className="my-4">
+          <input type="email" className="" placeholder="Enter Email" />
+          <button
+            className="bg-[#00df9a] w-[200px] rounded-md font-medium ml-4 my-6 px-6 py-3 text-black"
+            type="button"
+          >
+            Notify me
+          </button>
         </div>
       </div>
     </div>
