33b41f52cefe46d99e750a5d9cb4e130347853b2
MALWICK <batahumphrey66@gmail.com>
2023-02-26 09:59:14 +0100

    creating my footer  section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Analytics from './component/Analytics';
 import Cards from './component/Cards';
+import Footer from './component/Footer';
 import Hero from './component/Hero';
 import Navbar from './component/Navbar/Navbar';
 import Newsletter from './component/Newsletter';
@@ -14,6 +15,7 @@ function App() {
       <Analytics />
       <Newsletter />
       <Cards />
+      <Footer />
     </div>
   );
 }
